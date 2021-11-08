2f9e086f532cdfcaa01ac9e929976eb6b04582f3
personqianduixue <3480814463@qq.com>
2021-11-08 23:44:49 +0800

    update

--- code/problem3_2.m
+++ code/problem3_2.m
@@ -1,12 +1,19 @@
-R=300;
+clc,clear
 x=[];
-for x0=1:0.001:150
-    y0=-sqrt(R^2-x0^2);
-    n=[-x0,-y0];
-    e=[0,-1];
+R=300.4;
+for x0=-150:0.1:150
+    y0=-sqrt(R^2-x0.^2);
+    n=[-x0;-y0];
+    e=[0;-1];
     n1=n/norm(n);
     en=sum(e.*n1);
-    c=e-2*en*n1;
-    x=[x,c(1)/c(2)*(-0.534*R-y0)+x0];
+    c=e-2.*en.*n1;
+    x=[x,c(1)/c(2).*(-0.534*R-y0)+x0];
 end
-sum(abs(x)<0.5)/length(x)    
+x0=-150:0.1:150;
+plot(x0,x)   
+hold on
+y11=[0.5;-0.5].*ones(2,length(x0));
+plot(x0,y11,'r--')
+xlabel('入射点横坐标');ylabel('交点横坐标')
+beautiplot
--- code/solvex0.m
+++ code/solvex0.m
@@ -1,4 +1,10 @@
 syms x0 
 R=300.4;
-f=@(x0) (0.5+x0)/((sqrt(R^2-x0^2))-(1-0.466)*R)-(2*x0/(sqrt(R^2-x0^2)))/(1-x0^2/(sqrt(R^2-x0^2))^2)
-fsolve(f,150)
+f=@(x0) (-0.5+x0)/((sqrt(R^2-x0^2))-(1-0.466)*R)-(2*x0/(sqrt(R^2-x0^2)))/(1-x0^2/(sqrt(R^2-x0^2))^2)
+qvjian_start = 0;
+qvjian_end = 150;
+zero = [];
+for chu = [qvjian_start:1:qvjian_end]
+    zero = [zero,fzero(f,chu)];
+end
+zero = unique(zero)
